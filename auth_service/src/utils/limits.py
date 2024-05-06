cdbe10f45db660993549f2040f478d8daae6d958
Pavel Ershov <40699895+PavelErsh@users.noreply.github.com>
2024-05-06 20:59:26 +0200

    typing error fix

--- auth_service/src/utils/limits.py
+++ auth_service/src/utils/limits.py
@@ -2,13 +2,13 @@ import datetime
 from redis.asyncio import Redis
 from core.config import settings
 
- from typing import Optional
+from typing import Optional
 
 
 redis_conn = Redis(host=settings.redis_host, port=settings.redis_port)
 
 
- async def check_limit(user_id: str) -> Optional[bool]:
+async def check_limit(user_id: str) -> Optional[bool]:
      pipe = redis_conn.pipeline()
      now = datetime.datetime.now()
      key = f'{user_id}:{now.minute}'
